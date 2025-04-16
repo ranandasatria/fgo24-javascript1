56129bb499d4fab313dbdc7fb601b77d9eaa85f8
ranandasatria <rananda24@gmail.com>
2025-04-16 21:36:25 +0700

    Add files via upload

---
+++ switch-case.js
@@ -0,0 +1,12 @@
+
+const binatang = "ayam"
+switch(binatang){
+    case "ayam":
+        console.log("kukuruyuk")
+        break;
+    case "bebek":
+    console.log("wekwek")
+    break;
+    default:
+    console.log("binatang tidak dimiliki")
+}
