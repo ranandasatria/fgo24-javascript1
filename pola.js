f4ff4cdc147636fb1dd5b8b502bb4bf44bff9ff1
ranandasatria <rananda24@gmail.com>
2025-04-19 00:32:39 +0700

    Add files via upload

---
+++ pola.js
@@ -0,0 +1,25 @@
+for (let i = 0; i < 5; i++){
+    let wadah = ""
+    for (let j = 4; j >= 0; j--){
+        if ( j === i){
+            wadah += "  "
+        } else{
+            wadah += "* "
+        }
+    }
+    console.log(wadah)
+}
+
+
+for (let i = 0; i < 5; i++){
+    let wadah = ""
+    for (let j = 4; j >= 0; j--){
+        if ( j === i){
+            wadah += "* "
+        } else{
+            wadah += "  "
+        }
+    }
+    console.log(wadah)
+}
+
