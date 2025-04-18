55f684f698b9bb3f61bfdde0ef998f8802b47239
ranandasatria <rananda24@gmail.com>
2025-04-19 00:41:28 +0700

    Add files via upload

--- pola.js
+++ pola.js
@@ -1,3 +1,10 @@
+//   4 3 2 1
+// 0 * * * *    bintang = j > i
+// 1 * * *   *  spasi = j = i
+// 2 * *   * *
+// 3 *   * * *
+// 4   * * * *
+
 for (let i = 0; i < 5; i++){
     let wadah = ""
     for (let j = 4; j >= 0; j--){
